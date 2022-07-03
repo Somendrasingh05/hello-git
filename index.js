2531719b8b4e4cf481a6cc12966906f814570426
Somendrasingh05 <singhsomendra05092000@gmail.com>
2022-07-04 03:13:38 +0530

    js commited

---
+++ index.js
@@ -0,0 +1,44 @@
+// // starting javascript
+// // var let const
+
+// // variable declaration
+// var a; //here we dont declare type of variable like string int float ?
+
+// // variable initalization
+// a = 10;
+// console.log(a);
+// console.log(typeof a);
+// //reinitialization of a
+// a="hello";
+// console.log(a);
+// console.log(typeof a);  
+// // typeof operator -> it tells us the type od data stored in variable
+// a= true;
+// console.log(a);
+// console.log(typeof a);
+
+// a= null;
+// console.log(a);
+// console.log(typeof a);
+
+// Number
+var num = 10;
+console.log(num);
+var float = 2.4;
+console.log(float);
+
+//boolean
+var t= true;
+var f = false;
+console.log(t);
+console.log(f);
+
+//strings
+// we can use these 3 things to write string 
+'',"",``
+// single quotes, double quotes, backtick
+
+var str = 'a';
+console.log(typeof str);
+str = "How you doin ?"
+console.log(str);
